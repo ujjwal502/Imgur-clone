565e10249d0eda6844ff26da0e2aaa6233765ee6
Ujjwal <64697579+ujjwal502@users.noreply.github.com>
2020-07-22 00:47:59 +0530

    Delete keys.js

--- keys.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    MONGOURI: "mongodb+srv://ujjwal:5vdYI01bHBpmT3lx@cluster0.e2r33.mongodb.net/test?retryWrites=true&w=majority",
-    JWT_SECRET: "odjiojfkw8ffnci3rr4jfnf"
-}
-
